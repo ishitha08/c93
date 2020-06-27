f3d0039bd7c8f181242ad86fe0c72c2288897cce
ishitha08 <58905663+ishitha08@users.noreply.github.com>
2020-06-27 19:47:57 +0530

    Add files via upload

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import {View,Text,StyleSheet,Image,TextInput,TouchableOpacity,Alert,KeyboardAvoidingView} from 'react-native';
+import {View,Text,StyleSheet,Image,TextInput,TouchableOpacity,Alert,KeyboardAvoidingView,Modal} from 'react-native';
 import * as firebase from 'firebase'
 import db from './config';
+import { ScrollView } from 'react-native-gesture-handler';
 export default class LoginScreen extends React.Component {
 
     constructor(){
         super();
         this.state={
           emailId : '',
-          password: ''
+          password: '',
+          first_name:'',
+          last_name:'',
+          contact:'',
+          address:'',
+          confirm_password:''
         }
       }
     
@@ -37,9 +43,135 @@ export default class LoginScreen extends React.Component {
             Alert.alert('enter email and password');
         }
       }
+      userSignUp=(email,password,confirmPassword)=>{
 
+        if (email && password){
+          try{
+            const response = await firebase.auth().signInWithEmailAndPassword(email,password)
+            if(response){
+              this.props.navigation.navigate('Transaction')
+            }
+          }
+          catch(error){
+            switch (error.code) {
+              case 'auth/user-not-found':
+                Alert.alert("user dosen't exists")
+                console.log("doesn't exist")
+                break
+              case 'auth/invalid-email':
+                Alert.alert('incorrect email or password')
+                console.log('invaild')
+                break
+            }
+          }
+        }
+        else{
+            Alert.alert('enter email and password');
+        }
+      }
+  showModal=()=>{
+    return(
+    <View>
+      <Modal>
+        <ScrollView>
+          <KeyboardAvoidingView>
+          <TextInput
+          style={styles.loginBox}
+          placeholder="first name"
+          maxLength = {8}
+          onChangeText={(text)=>{
+            this.setState({
+              first_name: text
+            })
+          }}
+          value = {this.state.first_name}
+        />
+         <TextInput
+          style={styles.loginBox}
+          placeholder="last name"
+          maxLength = {8}
+          onChangeText={(text)=>{
+            this.setState({
+              last_name: text
+            })
+          }}
+          value = {this.state.last_name}
+        />
+         <TextInput
+          style={styles.loginBox}
+           placeholder="contact"
+          onChangeText={(text)=>{
+            this.setState({
+              contact: text
+            })
+          }}
+          value = {this.state.contact}
+        />
+         <TextInput
+          style={styles.loginBox}
+          multiline
+          maxLength = {1000}
+          placeholder="address"
+          onChangeText={(text)=>{
+            this.setState({
+              address: text
+            })
+          }}
+          value = {this.state.address}
+        />
+            <TextInput
+          style={styles.loginBox}
+          placeholder="abc@example.com"
+          keyboardType ='email-address'
+          onChangeText={(text)=>{
+            this.setState({
+              emailId: text
+            })
+          }}
+          value = {this.state.emailId}
+        />
+         <TextInput
+          style={styles.loginBox}
+          secureTextEntry = {true}
+          placeholder="enter Password"
+          onChangeText={(text)=>{
+            this.setState({
+              password: text
+            })
+          }}
+          value = {this.state.password}
+        />
+         <TextInput
+          style={styles.loginBox}
+          secureTextEntry = {true}
+          placeholder="confirm Password"
+          onChangeText={(text)=>{
+            this.setState({
+              confirm_password: text
+            })
+          }}
+          value = {this.state.confirm_password}
+        />
+          <TouchableOpacity style={{height:30,width:90,borderWidth:1,marginTop:20,paddingTop:5,borderRadius:7}}
+          onPress={()=>{this.userSignUp(this.state.emailId ,this.state.password,this.state.confirmPassword)}}>
+            <Text style={{textAlign:'center'}}>sign up</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={{height:30,width:90,borderWidth:1,marginTop:20,paddingTop:5,borderRadius:7}}
+          onPress={()=>{this.props.navigation.navigate(LoginScreen)}}>
+            <Text style={{textAlign:'center'}}>Cancel</Text>
+          </TouchableOpacity>
+          </KeyboardAvoidingView>
+        </ScrollView>
+      </Modal>
+    </View>
+    )
+  }
   render(){
       return(
+        <View>
+        {this.showModal()}
+       
         <KeyboardAvoidingView style = {{alignItems:'center',marginTop:20}}>
         <View>
           <Image
@@ -78,6 +210,7 @@ export default class LoginScreen extends React.Component {
 
         </View>
       </KeyboardAvoidingView>
+      </View>
 
       )
   }
